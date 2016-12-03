bd2f8dc4f720ce244153594cbed71552cdd09980
Sam <smohyee@gmail.com>
2016-12-03 18:54:02 -0500

    update memory :role property to make everyone engineers

--- role.engineer.js
+++ role.engineer.js
@@ -5,37 +5,37 @@
 Engineer role is meant to replace harvesters and upgraders.
 A single unit that harvests, builds, and upgrades the controller.
  */
-function roleEngineer(){
+var roleEngineer = {};
 
-    this.spawns = creep.room.find(FIND_MY_SPAWNS);
+roleEngineer.spawns = creep.room.find(FIND_MY_SPAWNS);
 
-    this.run = function(creep){
+roleEngineer.run = function(creep){
 
-        if(creep.memory.status == 'idle') creep.memory.status = this.determineStatus(creep);
-        if(creep.memory.status == 'harvesting') this.harvest(creep);
-        if(creep.memory.status == 'feeding spawn') this.feedSpawn(creep);
-        if(creep.memory.status == 'upgrading controller') this.upgradeCtrl(creep);
+    if(creep.memory.status == 'idle') creep.memory.status = this.determineStatus(creep);
+    if(creep.memory.status == 'harvesting') this.harvest(creep);
+    if(creep.memory.status == 'feeding spawn') this.feedSpawn(creep);
+    if(creep.memory.status == 'upgrading controller') this.upgradeCtrl(creep);
 
 
-    };
+};
 
-    this.determineStatus = function(creep){
-        //if out of energy, go harvest
-        if(creep.carry.energy == 0) return 'harvesting';
+roleEngineer.determineStatus = function(creep){
+    //if out of energy, go harvest
+    if(creep.carry.energy == 0) return 'harvesting';
 
-        //if spawns need energy, go feed
-        for(var i=0; i<this.spawns.length; i++){
-            if(this.spawns[i].energy < this.spawns[i].energyCapacity){
-                creep.memory.destinationID = this.spawns[i].id;
-                return 'feeding spawn';
-            }
+    //if spawns need energy, go feed
+    for(var i=0; i<this.spawns.length; i++){
+        if(this.spawns[i].energy < this.spawns[i].energyCapacity){
+            creep.memory.destinationID = this.spawns[i].id;
+            return 'feeding spawn';
         }
+    }
 
-        //otherwise, upgrade the controller
-        if(creep.room.controller.my) return 'upgrading controller';
-    };
+    //otherwise, upgrade the controller
+    if(creep.room.controller.my) return 'upgrading controller';
+};
 
-    this.harvest = function(creep){
+roleEngineer.harvest = function(creep){
         if(creep.carry.energy < creep.carryCapacity) {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
@@ -45,27 +45,25 @@ function roleEngineer(){
         else creep.memory.status = 'idle';
     };
 
-    this.feedSpawn = function(creep){
+roleEngineer.feedSpawn = function(creep){
 
-        var spawn = Game.getObjectById(creep.memory.destinationID);
+    var spawn = Game.getObjectById(creep.memory.destinationID);
 
-        if(creep.pos.isNearTo(spawn)){
-            if(creep.transfer(spawn, RESOURCE_ENERGY) == ERR_FULL) creep.memory.status = 'idle';
-        }
-        else creep.moveTo(spawn);
-    };
-
-    this.upgradeCtrl = function(creep){
-        var controller = creep.room.controller;
-
-        if(creep.pos.inRangeTo(controller, 3)){
-            if(creep.upgradeController(controller) == ERR_NOT_ENOUGH_RESOURCES) creep.memory.status = 'idle';
-        }
-        else creep.moveTo(controller);
-
+    if(creep.pos.isNearTo(spawn)){
+        if(creep.transfer(spawn, RESOURCE_ENERGY) == ERR_FULL) creep.memory.status = 'idle';
     }
-}
+    else creep.moveTo(spawn);
+};
 
+roleEngineer.upgradeCtrl = function(creep){
+    var controller = creep.room.controller;
+
+    if(creep.pos.inRangeTo(controller, 3)){
+        if(creep.upgradeController(controller) == ERR_NOT_ENOUGH_RESOURCES) creep.memory.status = 'idle';
+    }
+    else creep.moveTo(controller);
+
+}
 
 
 module.exports = roleEngineer;
