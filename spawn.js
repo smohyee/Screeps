133b76221c9b86cab78c93f23ec32708a9b63e7a
Sam <smohyee@gmail.com>
2016-12-03 05:19:30 -0500

    module for spawn

--- spawn.js
+++ spawn.js
@@ -9,7 +9,7 @@ var spawn = {
     //
     harvester: function(location){
         location.createCreep([WORK, CARRY, MOVE], null, {role: 'harvester'});
-    }
+    },
 
     /**
      * @param {StructureSpawn} location
