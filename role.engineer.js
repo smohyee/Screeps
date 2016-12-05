d6a4e3bbbdbe58768446fd65ce0d63acd02e363c
Sam <smohyee@gmail.com>
2016-12-05 13:33:15 -0500

    memory cleanup of removed containers

--- role.engineer.js
+++ role.engineer.js
@@ -108,7 +108,6 @@ var roleEngineer = {
             targetSite = Game.getObjectById(creep.memory.destinationID);
             if(creep.pos.getRangeTo(targetSite.pos) <= 3){
                 if(creep.build(targetSite) == ERR_NOT_ENOUGH_RESOURCES) creep.memory.status = 'idle';
-                creep.moveTo(targetSite);
             }
             else creep.moveTo(targetSite);
         }
