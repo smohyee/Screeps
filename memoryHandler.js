b0427a1ffc9186b1889ed9b53163d501f6c638e9
Sam <smohyee@gmail.com>
2016-12-04 20:54:08 -0500

    Trying to introduce Containers to memory storage

--- memoryHandler.js
+++ memoryHandler.js
@@ -1,6 +1,31 @@
 /**
  * Created by Sam on 12/4/2016.
  */
+Object.defineProperty(StructureContainer.prototype, 'memory', {
+    enumerable: true,
+    configurable: false,
+    get: function() {
+        if(_.isUndefined(Memory.containers)) {
+            Memory.containers = {};
+        }
+        if(!_.isObject(Memory.containers)) {
+            return undefined;
+        }
+        return Memory.containers[this.id] = Memory.containers[this.id] || {};
+    },
+    set: function(value) {
+        if(_.isUndefined(Memory.containers)) {
+            Memory.containers = {};
+        }
+        if(!_.isObject(Memory.containers)) {
+            throw new Error('Could not set container memory');
+        }
+        Memory.containers[this.id] = value;
+    }
+});
+
+
+
 var memoryHandler = {
 
     run: function(){
@@ -28,29 +53,6 @@ var memoryHandler = {
 */
     setContainerMemory: function(){
 
-
-        Object.defineProperty(StructureContainer.prototype, 'memory', {
-            enumerable: true,
-            configurable: false,
-            get: function() {
-                if(_.isUndefined(Memory.containers)) {
-                    Memory.containers = {};
-                }
-                if(!_.isObject(Memory.containers)) {
-                    return undefined;
-                }
-                return Memory.containers[this.id] = Memory.containers[this.id] || {};
-            },
-            set: function(value) {
-                if(_.isUndefined(Memory.containers)) {
-                    Memory.containers = {};
-                }
-                if(!_.isObject(Memory.containers)) {
-                    throw new Error('Could not set container memory');
-                }
-                Memory.containers[this.id] = value;
-            }
-        });
     }
 };
 
